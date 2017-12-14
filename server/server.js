05fcf85724df82190fef0c4f82acdfcfcd31883e
s-r-aman <s.r.aman.sra@gmail.com>
2017-12-14 18:37:56 +0530

    Initail commit.

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const port = process.env.PORT ||3000;
+
+app.use(express.static(publicPath));
+
+// app.get('/',(req, res) =>{
+//     res.send('index.html');
+// });
+
+app.listen(port, () => {
+    console.log('Server is up on port:', port);
+})
